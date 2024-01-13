57a3722ca0f231f182587a1237e7a1cfd817963b
Cheltsov Kostiantyn <k.cheltsov@m2e.team>
2024-01-13 19:35:41 +0200

    Initial commit 2

---
+++ src/Command/Exchange/BankAbstract.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command\Exchange;
+
+abstract class BankAbstract
+{
+    protected array $exchangeRates = [];
+
+    abstract protected function parseExchangeRates(array $response): array;
+
+    /**
+     * @param array{USD: array{buy: float, sell: float}, EUR: array{buy: float, sell: float}} $exchangeRates
+     */
+    public function setExchangeRates(array $exchangeRates): self
+    {
+        $this->exchangeRates = $exchangeRates;
+
+        return $this;
+    }
+
+    /**
+     * @return array{USD: array{buy: float, sell: float}, EUR: array{buy: float, sell: float}}
+     */
+    public function getExchangeRates(): array
+    {
+        return $this->exchangeRates;
+    }
+}
